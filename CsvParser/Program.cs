5ace3ce8aa1ab922da79a3afb54d5f8e36f315a4
Mingjing Yu <mingjingyu@microsoft.com>
2022-06-07 14:10:44 +0800

    .

--- CsvParser/Program.cs
+++ CsvParser/Program.cs
@@ -9,16 +9,17 @@ namespace CsvParser
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var csv = @"Year,Title,Production Studio
-                        2008,Iron Man,Marvel Studios
-                        2008,The Incredible Hulk,Marvel Studios
-                        2008,Punisher: War Zone,Marvel Studios
-                        2009,X-Men Origins: Wolverine,20th Century Fox
-                        2010,Iron Man 2,Marvel Studios
-                        2011,Thor,Marvel Studios
-                        2011,X-Men: First Class,20th Century Fox";
-            var sr = new StringReader(csv);
-            var csvReader = new CsvReader(sr);
+            // var csv = @"Year,Title,Production Studio
+            //             2008,Iron Man,Marvel Studios
+            //             2008,The Incredible Hulk,Marvel Studios
+            //             2008,Punisher: War Zone,Marvel Studios
+            //             2009,X-Men Origins: Wolverine,20th Century Fox
+            //             2010,Iron Man 2,Marvel Studios
+            //             2011,Thor,Marvel Studios
+            //             2011,X-Men: First Class,20th Century Fox";
+            // var reader = new StringReader(csv);
+            var reader = new StreamReader(new FileStream("Marvel.csv", FileMode.Open));
+            var csvReader = new CsvReader(reader);
             foreach (var line in csvReader.Lines)
             {
                 Console.WriteLine(line.First(p => p.Key == "Title").Value);
