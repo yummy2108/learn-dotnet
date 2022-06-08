063e49a0738c8b52667d3966bae192e18a101d9e
Mingjing Yu <mingjingyu@microsoft.com>
2022-06-08 17:16:23 +0800

    .

---
+++ SqliteConsoleTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace SqliteConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (var connection = new SqliteConnection("Data Source=:memory:"))
+            {
+                connection.Open();
+                var command = new SqliteCommand("SELECT 1;", connection);
+                long result = (long)command.ExecuteScalar();
+                Console.WriteLine($"Command output: {result}");
+            }
+        }
+    }
+}
