3900eb07a0e403fecb68b4810c4f8ccea5887e52
Mingjing Yu <mingjingyu@microsoft.com>
2022-06-08 15:21:00 +0800

    .

---
+++ CsvParser/CsvReaderTest.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace CsvParser
+{
+    public class CsvReaderTest : IDisposable
+    {
+        private StreamReader streamReader;
+        private CsvReader csvReader;
+
+        public CsvReaderTest()
+        {
+            streamReader = new StreamReader(new FileStream("Marvel.csv", FileMode.Open));
+            csvReader = new CsvReader(streamReader);
+        }
+
+        public void Dispose()
+        {
+            streamReader.Dispose();
+        }
+
+        [Fact]
+        public void VerifyNumberOfLines()
+        {
+            Assert.Equal(7, csvReader.Lines.Count());
+        }
+    }
+}
