d1c5cea18d848e08abcc6ee4d5cfd9d907f3aeca
Mingjing Yu <mingjingyu@microsoft.com>
2022-06-14 17:59:58 +0800

    .

--- Ch6/DapperDi/ScmDataAccess/DataAccessFactory.cs
+++ Ch6/DapperDi/ScmDataAccess/DataAccessFactory.cs
@@ -2,27 +2,38 @@ using System;
 using System.Linq;
 using System.Reflection;
 
-enum DatabaseType {
-    Sqlite,
-    SqlServer,
-    PostgerSql
-}
 
 static class DataAccessFactory
 {
-    internal static string ConnectionString { get; set; }
-    internal static IScmContext GetScmContext(DatabaseType dbType)
-    {
-        switch (dbType)
-        {
-            case DatabaseType.Sqlite:
-                return new SqliteScmContext(ConnectionString);
-            case DatabaseType.SqlServer:
-                return new SqlServerScmContext(ConnectionString);
-            case DatabaseType.PostgerSql:
-                return new PostgreSqlScmContext(ConnectionString);
-            default:
-                throw new ArgumentException($"Unrecognized Database type {dbType}", "dbType");
+    internal static Type scmContextType = null;
+    internal static Type ScmContextType{
+        get { return scmContextType; }
+        set {
+            if (!value.GetTypeInfo().ImplementedInterfaces.Contains(typeof(IScmContext))){
+                throw new ArgumentException(
+                    $"{value.GetTypeInfo().FullName} doesn't implement IScmContext"
+                );
+            }
+            scmContextType = value;
         }
+    }
+    internal static IScmContext GetScmContext()
+    {
+        // switch (dbType)
+        // {
+        //     case DatabaseType.Sqlite:
+        //         return new SqliteScmContext(ConnectionString);
+        //     case DatabaseType.SqlServer:
+        //         return new SqlServerScmContext(ConnectionString);
+        //     case DatabaseType.PostgerSql:
+        //         return new PostgreSqlScmContext(ConnectionString);
+        //     default:
+        //         throw new ArgumentException($"Unrecognized Database type {dbType}", "dbType");
+        // }
+
+        if (scmContextType == null) {
+            throw new ArgumentNullException("ScmContextType not set");
+        }
+        return Activator.CreateInstance(scmContextType) as IScmContext;
     }
 }
---
+++ Ch6/DapperDi/SqliteDal/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SqliteDal
+{
+    public class Class1
+    {
+    }
+}
